0c68282573239136a22ce41d072070f402ea9731
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-09-03 12:19:58 +0200

    list of links for one page

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import selenium
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 import requests
 import lxml
 from bs4 import BeautifulSoup
+import time
 
 
 base_url = "https://www.immoweb.be/"
@@ -11,6 +17,22 @@ base_url = "https://www.immoweb.be/"
 #       - House
 #       - Apart
 # /!\ without life sales
+
+
+
+# Aparts
+    # WIP list of link by pages
+        #TODO 
+        # Take info
+        # save as one file
+# Houses
+    # WIP list of link by pages
+        #TODO 
+        # Take info
+        # save as one file
+# concat files
+
+
 page_number = 1
 
 url_appart_search = "{}fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
@@ -20,11 +42,31 @@ print(url_appart_search)
 appart_links = []
 house_links = []
 
-response = requests.get(url_appart_search)
+driver = webdriver.Firefox()
+driver.implicitly_wait(10)
+driver.get(url_appart_search)
 
-if response.status_code >= 200 and response.status_code <= 400:
-    soup = BeautifulSoup(response.content, "lxml")
-    print(soup)
+assert "Immoweb" in driver.title
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'uc-btn-accept-banner'))
+    )
+    if element is not None:
+        element.click()
+        print("button clicked")
+except Exception as e:
+    print(e)
+
+soup = BeautifulSoup(driver.page_source, "lxml")
+intermediate_links = soup.find_all("a", {"class" : "card__title-link"})
+for link in intermediate_links:
+    link = link.get("href")
+    appart_links.append(link)
+
+print(appart_links)
+
+driver.close()
+
 
 
 
