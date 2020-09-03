a3314bdd5b891148e51f80c7a85497a15ec53e9c
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-09-03 12:26:02 +0200

    setup for pages' range

--- main.py
+++ main.py
@@ -21,51 +21,46 @@ base_url = "https://www.immoweb.be/"
 
 
 # Aparts
-    # WIP list of link by pages
-        #TODO 
-        # Take info
-        # save as one file
-# Houses
-    # WIP list of link by pages
-        #TODO 
-        # Take info
-        # save as one file
-# concat files
-
-
-page_number = 1
-
-url_appart_search = "{}fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
-url_house_search = "{}fr/recherche/maison/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
-print(url_appart_search)
+    # DONE list of link by pages
+        # WIP Take info
+        # TODO save as one file
+# TODO Houses
+    # TODO list of link by pages
+        #TODO Take info
+        #TODO  save as one file
+# TODO concat files
 
 appart_links = []
 house_links = []
 
-driver = webdriver.Firefox()
-driver.implicitly_wait(10)
-driver.get(url_appart_search)
+for page_number in range (1,2):
 
-assert "Immoweb" in driver.title
-try:
-    element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, 'uc-btn-accept-banner'))
-    )
-    if element is not None:
-        element.click()
-        print("button clicked")
-except Exception as e:
-    print(e)
+    url_appart_search = "{}fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
+    url_house_search = "{}fr/recherche/maison/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
+    
+    driver = webdriver.Firefox()
+    driver.implicitly_wait(10)
+    driver.get(url_appart_search)
+    assert "Immoweb" in driver.title
+    try:
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.ID, 'uc-btn-accept-banner'))
+        )
+        if element is not None:
+            element.click()
+            print("button clicked")
+    except Exception as e:
+        print(e)
 
-soup = BeautifulSoup(driver.page_source, "lxml")
-intermediate_links = soup.find_all("a", {"class" : "card__title-link"})
-for link in intermediate_links:
-    link = link.get("href")
-    appart_links.append(link)
+    soup = BeautifulSoup(driver.page_source, "lxml")
+    intermediate_links = soup.find_all("a", {"class" : "card__title-link"})
+    for link in intermediate_links:
+        link = link.get("href")
+        appart_links.append(link)
 
-print(appart_links)
+    print(appart_links)
 
-driver.close()
+    driver.close()
 
 
 
@@ -86,4 +81,4 @@ driver.close()
 
 
 # TODO
-# Panda to organize everything in a goot csv
+# Panda to organize everything in a good csv
