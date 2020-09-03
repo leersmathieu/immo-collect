87e3c3645f79c0ef62c9c6f28a8b26578d194727
Mathieu <leers.mathieu@gmail.com>
2020-09-03 10:58:31 +0200

    init scrapping

--- main.py
+++ main.py
@@ -1,11 +1,35 @@
-# init file
+import selenium
+import requests
+import lxml
+from bs4 import BeautifulSoup
+
+
+base_url = "https://www.immoweb.be/"
 
 # TODO
 # 2 searches with Selenium :
 #       - House
 #       - Apart
 # /!\ without life sales
+page_number = 1
 
+url_appart_search = "{}fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
+url_house_search = "{}fr/recherche/maison/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
+print(url_appart_search)
+
+appart_links = []
+house_links = []
+
+response = requests.get(url_appart_search)
+
+if response.status_code >= 200 and response.status_code <= 400:
+    soup = BeautifulSoup(response.content, "lxml")
+    print(soup)
+
+
+
+# TODO
+# make a for in range (333?)
 
 # TODO
 # For each search : (333 pages each) (Selenium)
