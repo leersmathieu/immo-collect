4bcfe82aa4a2dd9b37e22d53353895cd8a1a04cc
Morgane Demesmaeker <morgane.demesmaeker@gmail.com>
2020-09-03 16:44:06 +0200

    lot of comments and more

--- main.py
+++ main.py
@@ -4,10 +4,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import requests
 import lxml
-from bs4 import BeautifulSoup
+
 import time
+import re
 
 
 base_url = "https://www.immoweb.be/"
@@ -18,6 +20,9 @@ base_url = "https://www.immoweb.be/"
 #       - Apart
 # /!\ without life sales
 
+######################################
+#  Get the urls of the search pages  #
+######################################
 
 
 # Aparts
@@ -34,16 +39,24 @@ appart_links = []
 house_links = []
 
 property_type = ["Appartment", "House"]
+avendretext = '.+( à vendre)'
 
 for page_number in range (1,2):
 
+    ######################################
+    #     Get the urls of each pages     #
+    ######################################
     url_appart_search = "{}fr/recherche/appartement/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
     url_house_search = "{}fr/recherche/maison/a-vendre?countries=BE&isALifeAnnuitySale=false&page={}&orderBy=relevance".format(base_url, page_number)
     
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.implicitly_wait(10)
     driver.get(url_appart_search)
+
+    # check existence of the page
     assert "Immoweb" in driver.title
+
+    # take away the popup
     try:
         element = WebDriverWait(driver, 10).until(
             EC.presence_of_element_located((By.ID, 'uc-btn-accept-banner'))
@@ -54,44 +67,51 @@ for page_number in range (1,2):
     except Exception as e:
         print(e)
 
+    # take all the links
     soup = BeautifulSoup(driver.page_source, "lxml")
     intermediate_links = soup.find_all("a", {"class": "card__title-link"})
     for link in intermediate_links:
         link = link.get("href")
         appart_links.append(link)
 
+        ######################################
+        #    Get the infos of each pages     #
+        ######################################
+
     for url in appart_links:
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, "lxml")
+        
         postal_code = driver.find_element_by_css_selector("span.classified__information--address-row > span")
         city = driver.find_element_by_css_selector("span.classified__information--address-row > span + span + span")
+        
+        # TODO au lieu de regarder le titre, regarder le prix
+        # si prix =+ range => lot, et chercher le subtype dans le "tous les biens", ainsi que prix
+                        # sauf si pas "tous les biens" => même si range, pas un lot
+
+        alone = True    # == pas un lot
         property_subtype = driver.find_element_by_css_selector("h1.classified__title")
+        property_subtype = property_subtype.text
+
+        if alone and re.match(avendretext, property_subtype):
+            property_subtype = property_subtype[:-9]
+
 
         print(postal_code.text)
         print(city.text)
         print(property_type[0])
-        print(property_subtype.text)
+        print(property_subtype)
         driver.close()
         break
 
 
+        ######################################
+        #    Save the infos of each pages    #
+        ######################################
 
 
 
-# TODO
-# make a for in range (333?)
+######################################
+#         Concat all the csv         #
+######################################
 
-# TODO
-# For each search : (333 pages each) (Selenium)
-#       For each pages :
-#               For each house/apart :
-#                       Take link (2 lists, one for house, one for apart)
-
-
-# TODO
-# For each link :
-#       Take information (BeautifulSoup)
-
-
-# TODO
-# Panda to organize everything in a good csv
