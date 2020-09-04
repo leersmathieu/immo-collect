b89ce6b53ea5c17a2f7a3b0caa8ea1e7f6e2e5af
mpietquin <michaelpietquin@gmail.com>
2020-09-04 08:05:17 +0200

    min-max price and is batch

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import glob
 import os
 import time
@@ -23,26 +24,27 @@ base_url = "https://www.immoweb.be/"
 #  Get the urls of the search pages  #
 ######################################
 
-
+"""
 # Aparts
-# DONE list of link by pages
-# WIP Take info
-# TODO save as one file
-# TODO Houses
-# TODO list of link by pages
-# TODO Take info
-# TODO  save as one file
-# TODO concat files
+    # DONE list of link by pages
+    # WIP Take info
+    # TODO save as one file
+    # TODO Houses
+    # TODO list of link by pages
+    # TODO Take info
+    # TODO  save as one file
+    # TODO concat files
+"""
 
 appart_links = []
 house_links = []
 
-property_type = ["Appartment", "House"]
-avendretext = '.+( à vendre)'
+property_type = ["Appartement", "Maison"]
+avendretext = re.compile('.+( à vendre)')
 
 # make sure the path is created for csv
-if not os.path.exists("data"):
-    os.mkdir("data")
+if not os.path.exists("immo-data"):
+    os.mkdir("immo-data")
 
 ######################################
 #     Get the urls of each page      #
@@ -67,32 +69,30 @@ try:
         print("button clicked")
 except Exception as e:
     print(e)
+# current search between 'appartement' and 'maison'
+current_search_id = 0
+current_url = url_appart_search if current_search_id == 0 else url_house_search
 # max pages to process
 nb_pages = 1  # should be 333
 for page_number in range(nb_pages):
-    driver.get(url_appart_search.format(page_number))
+    driver.get(current_url.format(page_number))
     time.sleep(4)
     # take all the links
     soup = BeautifulSoup(driver.page_source, "lxml")
     intermediate_links = soup.find_all("a", {"class": "card__title-link"})
-    for link in intermediate_links:
-        link = link.get("href")
-        appart_links.append(link)
+    collected_links = [link.get("href") for link in intermediate_links]
 
-        ######################################
-        #    Get the infos of each pages     #
-        ######################################
+    ######################################
+    #    Get the infos of each pages     #
+    ######################################
 
-    for url in appart_links:
+    for url in collected_links:
+        (appart_links if current_search_id == 0 else house_links).append(url)
         driver.get(url)
         soup = BeautifulSoup(driver.page_source, "lxml")
 
         postal_code = driver.find_element_by_css_selector("span.classified__information--address-row > span")
         city = driver.find_element_by_css_selector("span.classified__information--address-row > span + span + span")
-
-        # TODO au lieu de regarder le titre, regarder le prix
-        # si prix =+ range => lot, et chercher le subtype dans le "tous les biens", ainsi que prix
-        # sauf si pas "tous les biens" => même si range, pas un lot
 
         alone = True  # == pas un lot
         property_subtype = driver.find_element_by_css_selector("h1.classified__title")
@@ -101,13 +101,29 @@ for page_number in range(nb_pages):
         if alone and re.match(avendretext, property_subtype):
             property_subtype = property_subtype[:-9]
 
-        price = soup.find("p", attrs={"class": "classified__price"}).find("span").find("span").text[:-2]
+        price = soup.find("p", attrs={"class": "classified__price"}).find("span").find("span").text
+        is_batch = False
+        splits = price.split(" - ")
+        if len(splits) == 1:
+            min_price = max_price = price
+        # si prix =+ range => lot
+        else:
+            min_price = splits[0]
+            max_price = splits[1]
+            # cherche le subtype dans "tous les biens"
+            try:
+                tous_les_biens = soup.find("section", attrs={"class": "classified__cluster"}).find("div")\
+                    .find("div").find("div").find("div").find("div").find("div").text.strip()
+                is_batch = len(tous_les_biens) > 0
+            except AttributeError:
+                pass
 
         print("Postal Code: {}".format(postal_code.text))
         print("City: {}".format(city.text))
-        print("Type of property: {}".format(property_type[0]))
-        print("Property Subtype: ".format(property_subtype))
-        print("Price: {}".format(str(price)))
+        print("Type of property: {}".format(property_type[current_search_id]))
+        print("Property Subtype: {}".format(property_subtype))
+        print("Min Price: {} - Max Price: {}".format(min_price, max_price))
+        print("Is a batch: {}".format(is_batch))
 
         print("Type of sale: TODO")
         print("Number of rooms: TODO")
@@ -126,17 +142,17 @@ for page_number in range(nb_pages):
         # TODO remove me (intend to break the loop for current tests)
         break
 
-driver.close()
+    ######################################
+    #    Save the infos of each pages    #
+    ######################################
 
-        ######################################
-        #    Save the infos of each pages    #
-        ######################################
+driver.close()
 
 ######################################
 #         Concat all the csv         #
 ######################################
 def concat_all_CSV():
-    all_files = glob.glob("./data/*.csv")
+    all_files = glob.glob("./immo-data/*.csv")
     df = pd.concat([pd.read_csv(filename, index_col=None, header=0) for filename in all_files], axis=0, ignore_index=True)
     # saved in the current folder
     df.to_csv("immo_collect.csv")
