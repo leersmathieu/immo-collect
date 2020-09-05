577f5ffe50519eaef1f3c6b38aafcdad1a3d1144
mpietquin <michaelpietquin@gmail.com>
2020-09-05 19:48:57 +0200

    re-add a little of logic in the main

--- function.py
+++ function.py
@@ -1,11 +1,12 @@
 # init file
 import glob
 import os
-from typing import List, Dict
+from typing import List, Dict, Union
 
 import pandas as pd
 import re
 
+from pandas import DataFrame
 from selenium.common.exceptions import NoSuchElementException
 from selenium import webdriver
 from bs4 import BeautifulSoup
@@ -70,230 +71,209 @@ def init_connection(driver: webdriver, url, title: str = None, check_button: boo
             print(e)
 
 
-def collect_links(driver: webdriver, page_number: int, current_search_id: int) -> List[str]:
-    # target the right page
-    driver.get((url_appart_search if current_search_id == 0 else url_house_search).format(page_number))
-    # take all the links
+def collect_info(driver: webdriver, url: str, current_search_id: int) -> Dict:
+    ######################################
+    #    Get the infos of a page         #
+    ######################################
+    driver.get(url)
+
     soup = BeautifulSoup(driver.page_source, "lxml")
-    intermediate_links = soup.find_all("a", {"class": "card__title-link"})
-    return [link.get("href") for link in intermediate_links]
 
+    # cherche le subtype dans "tous les biens" et skip si pas vide
+    # (les différents éléments des lots sont pris séparément)
+    if get_bool_presence("h2", "text-block__title", "Tous les biens", soup):
+        return {}
 
-def load_page(driver: webdriver, page_number: int, current_search_id: int) -> List[Dict]:
+    #####################
+    #   Instanciations  #
+    #####################
 
-    collected_links = collect_links(driver, page_number, current_search_id)
+    # Base
+    vente_publique = get_bool_presence("h2", "text-block__title", "Vente publique", soup)
+    rapport = get_bool_presence("th", "classified-table__header", "Immeuble de rapport", soup)
+    bien_neuf = get_bool_presence("span", "flag-list__text", "Nouvelle construction", soup)
+    postal_code = None
+    city = None
+    property_subtype = None
 
-    ######################################
-    #    Get the infos of each pages     #
-    ######################################
+    # Général
+    facade = None
+    etat_batiment = None
 
-    donnees = []
+    # Intérieur
+    area = None
+    chamber = None
+    cuisine_equipe = None
+    feu_ouvert = False
+    meuble = False
 
-    for url in collected_links:
-        driver.get(url)
-        soup = BeautifulSoup(driver.page_source, "lxml")
+    # Extérieur
+    jardin = False
+    surface_jardin = None
+    terrasse = False
+    surface_terrasse = None
+    surface_terrain = None
 
-        # cherche le subtype dans "tous les biens" et skip si pas vide
-        # (les différents éléments des lots sont pris séparément)
-        if get_bool_presence("h2", "text-block__title", "Tous les biens", soup):
-            continue
+    # Installations
+    piscine = False
 
-        #####################
-        #   Instanciations  #
-        #####################
+    # Urbanisme
+    surface_constructible = None
 
-        # Base
-        vente_publique = get_bool_presence("h2", "text-block__title", "Vente publique", soup)
-        rapport = get_bool_presence("th", "classified-table__header", "Immeuble de rapport", soup)
-        bien_neuf = get_bool_presence("span", "flag-list__text", "Nouvelle construction", soup)
-        postal_code = None
-        city = None
-        property_subtype = None
+    # Finances
+    price = None
+
+    #####################
+    #   Informations    #
+    #####################
+
+    # Base
+    postal_code = re.search(search_for_postal_code, url).group("postal_code")
+    try:
+        city = driver.find_element_by_css_selector("p.classified__information--address-clickable").text.split(" — ")[
+                -1].strip()
+    except NoSuchElementException:
+        # fallback
+        city = driver.find_element_by_css_selector("span.classified__information--address-row").text.split(" — ")[
+                -1].replace("|", "").strip()
+    property_subtype = driver.find_element_by_css_selector("h1.classified__title")
+    property_subtype = property_subtype.text
+
+    if re.match(avendretext, property_subtype):
+        property_subtype = property_subtype[:-9]
+
+    accordion = soup.find_all('div', {"class": "accordion accordion--section"})
+
+    for elem in accordion:
+        entete = elem.find("h2").text
 
         # Général
-        facade = None
-        etat_batiment = None
+        if entete == "Général":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                trs = line.find_all("tr")
+                for tr in trs:
+                    th = tr.find("th").text.strip()
+
+                    if th.startswith("Façades"):
+                        facade = int(tr.find("td").text.strip())
+
+                    elif th.startswith("État du bâtiment"):
+                        etat_batiment = tr.find("td").text.strip()
 
         # Intérieur
-        area = None
-        chamber = None
-        cuisine_equipe = None
-        feu_ouvert = False
-        meuble = False
+        elif entete == "Intérieur":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                trs = line.find_all("tr")
+                for tr in trs:
+                    th = tr.find("th").text.strip()
+
+                    if th.startswith("Surface habitable"):
+                        area = tr.find("td").text.split()
+                        area = int(area[0])
+
+                    elif th.startswith("Chambres"):
+                        chamber = int(tr.find("td").text.strip())
+
+                    elif th.startswith("Feu ouvert"):
+                        feu_ouvert = True
+
+                    elif th.startswith("Type de cuisine"):
+                        cuisine_equipe = tr.find("td").text.strip()
+
+                    elif th.startswith("Meublé"):
+                        meuble = True
 
         # Extérieur
-        jardin = False
-        surface_jardin = None
-        terrasse = False
-        surface_terrasse = None
-        surface_terrain = None
+        elif entete == "Extérieur":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                trs = line.find_all("tr")
+                for tr in trs:
+                    th = tr.find("th").text.strip()
+
+                    if th.startswith("Surface du jardin"):
+                        surface_jardin = tr.find("td").text.split()
+                        surface_jardin = int(surface_jardin[0])
+                        if surface_jardin > 0:
+                            jardin = True
+                    elif th.startswith("Jardin"):
+                        jardin = True
+
+                    elif th.startswith("Surface de la terrasse"):
+                        surface_terrasse = tr.find("td").text.split()
+                        surface_terrasse = int(surface_terrasse[0])
+                        if surface_terrasse > 0:
+                            terrasse = True
+                    elif th.startswith("Terrasse"):
+                        terrasse = True
+
+                    elif th.startswith("Surface du terrain"):
+                        surface_terrain = tr.find("td").text.split()
+                        surface_terrain = int(surface_terrain[0])
 
         # Installations
-        piscine = False
+        elif entete == "Installations":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                trs = line.find_all("tr")
+                for tr in trs:
+                    th = tr.find("th").text.strip()
+
+                    if th.startswith("Piscine"):
+                        piscine = True
 
         # Urbanisme
-        surface_constructible = None
+        elif entete == "Urbanisme":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                trs = line.find_all("tr")
+                for tr in trs:
+                    th = tr.find("th").text.strip()
+
+                    if th.startswith("Surface constructible"):
+                        surface_constructible = tr.find("td").text.split()
+                        surface_constructible = int(surface_constructible[0])
 
         # Finances
-        price = None
+        elif entete == "Finances":
+            lines = elem.find_all("div", {"class": "accordion__content"})
+            for line in lines:
+                span = line.find_all("span", {"class": "sr-only"})
+                try:
+                    price = span[0].text.replace("€", "").strip()
+                except IndexError:
+                    # fallback
+                    price = soup.find("p", {"class": "classified__price"}).find("span", {
+                        "class": "sr-only"}).text.replace("€", "").strip()
 
-        #####################
-        #   Informations    #
-        #####################
-
-        # Base
-        postal_code = search = re.search(search_for_postal_code, url).group("postal_code")
-        try:
-            city = \
-                driver.find_element_by_css_selector("p.classified__information--address-clickable").text.split(" — ")[
-                    -1].strip()
-        except NoSuchElementException:
-            # fallback
-            city = \
-                driver.find_element_by_css_selector("span.classified__information--address-row").text.split(" — ")[
-                    -1].replace("|", "").strip()
-        property_subtype = driver.find_element_by_css_selector("h1.classified__title")
-        property_subtype = property_subtype.text
-
-        if re.match(avendretext, property_subtype):
-            property_subtype = property_subtype[:-9]
-
-        accordion = soup.find_all('div', {"class": "accordion accordion--section"})
-
-        for elem in accordion:
-            entete = elem.find("h2").text
-
-            # Général
-            if entete == "Général":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    trs = line.find_all("tr")
-                    for tr in trs:
-                        th = tr.find("th").text.strip()
-
-                        if th.startswith("Façades"):
-                            facade = int(tr.find("td").text.strip())
-
-                        elif th.startswith("État du bâtiment"):
-                            etat_batiment = tr.find("td").text.strip()
-
-            # Intérieur
-            elif entete == "Intérieur":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    trs = line.find_all("tr")
-                    for tr in trs:
-                        th = tr.find("th").text.strip()
-
-                        if th.startswith("Surface habitable"):
-                            area = tr.find("td").text.split()
-                            area = int(area[0])
-
-                        elif th.startswith("Chambres"):
-                            chamber = int(tr.find("td").text.strip())
-
-                        elif th.startswith("Feu ouvert"):
-                            feu_ouvert = True
-
-                        elif th.startswith("Type de cuisine"):
-                            cuisine_equipe = tr.find("td").text.strip()
-
-                        elif th.startswith("Meublé"):
-                            meuble = True
-
-            # Extérieur
-            elif entete == "Extérieur":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    trs = line.find_all("tr")
-                    for tr in trs:
-                        th = tr.find("th").text.strip()
-
-                        if th.startswith("Surface du jardin"):
-                            surface_jardin = tr.find("td").text.split()
-                            surface_jardin = int(surface_jardin[0])
-                            if surface_jardin > 0:
-                                jardin = True
-                        elif th.startswith("Jardin"):
-                            jardin = True
-
-                        elif th.startswith("Surface de la terrasse"):
-                            surface_terrasse = tr.find("td").text.split()
-                            surface_terrasse = int(surface_terrasse[0])
-                            if surface_terrasse > 0:
-                                terrasse = True
-                        elif th.startswith("Terrasse"):
-                            terrasse = True
-
-                        elif th.startswith("Surface du terrain"):
-                            surface_terrain = tr.find("td").text.split()
-                            surface_terrain = int(surface_terrain[0])
-
-            # Installations
-            elif entete == "Installations":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    trs = line.find_all("tr")
-                    for tr in trs:
-                        th = tr.find("th").text.strip()
-
-                        if th.startswith("Piscine"):
-                            piscine = True
-
-            # Urbanisme
-            elif entete == "Urbanisme":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    trs = line.find_all("tr")
-                    for tr in trs:
-                        th = tr.find("th").text.strip()
-
-                        if th.startswith("Surface constructible"):
-                            surface_constructible = tr.find("td").text.split()
-                            surface_constructible = int(surface_constructible[0])
-
-            # Finances
-            elif entete == "Finances":
-                lines = elem.find_all("div", {"class": "accordion__content"})
-                for line in lines:
-                    span = line.find_all("span", {"class": "sr-only"})
-                    try:
-                        price = span[0].text.replace("€", "").strip()
-                    except IndexError:
-                        # fallback
-                        price = soup.find("p", {"class": "classified__price"}).find("span", {
-                            "class": "sr-only"}).text.replace("€", "").strip()
-
-        data = {
-            "Lien": url,
-            "Prix": price,
-            "Type de propriété": property_type[current_search_id],
-            "Vente publique": vente_publique,
-            "Immeuble de rapport": rapport,
-            "Bien neuf": bien_neuf,
-            "Code Postal": postal_code,
-            "Ville": city,
-            "Sous-type de propriété": property_subtype,
-            "Nombre de façades": facade,
-            "Etat du bâtiment": etat_batiment,
-            "Surface habitable": area,
-            "Nombre de chambre(s)": chamber,
-            "Type de cuisine": cuisine_equipe,
-            "Feu ouvert": feu_ouvert,
-            "Meublé": meuble,
-            "Jardin": jardin,
-            "Surface du jardin": surface_jardin,
-            "Terrasse": terrasse,
-            "Surface de la terrasse": surface_terrasse,
-            "Surface totale du terrain": surface_terrain,
-            "Piscine": piscine,
-            "Surface de la zone constructible": surface_constructible
-        }
-
-        print(data)
-
-        donnees.append(data)
-    return donnees
+    data = {
+        "Lien": url,
+        "Prix": price,
+        "Type de propriété": property_type[current_search_id],
+        "Vente publique": vente_publique,
+        "Immeuble de rapport": rapport,
+        "Bien neuf": bien_neuf,
+        "Code Postal": postal_code,
+        "Ville": city,
+        "Sous-type de propriété": property_subtype,
+        "Nombre de façades": facade,
+        "Etat du bâtiment": etat_batiment,
+        "Surface habitable": area,
+        "Nombre de chambre(s)": chamber,
+        "Type de cuisine": cuisine_equipe,
+        "Feu ouvert": feu_ouvert,
+        "Meublé": meuble,
+        "Jardin": jardin,
+        "Surface du jardin": surface_jardin,
+        "Terrasse": terrasse,
+        "Surface de la terrasse": surface_terrasse,
+        "Surface totale du terrain": surface_terrain,
+        "Piscine": piscine,
+        "Surface de la zone constructible": surface_constructible
+    }
+    return data
 
 
 def save_data(donnees: List[Dict], page_number, current_search_id: int):
@@ -310,13 +290,15 @@ def save_data(donnees: List[Dict], page_number, current_search_id: int):
         print("Skipping to store an empty data for page {}".format(page_number))
 
 
-def concat_all_csv(all_files_path: str, recursive=False) -> pd.DataFrame:
+def concat_all_csv(all_files_path: str, recursive=False) -> Union[DataFrame, None]:
     """
     Fonction to concat a list of files into a single csv
     """
     all_files = glob.glob(all_files_path, recursive=recursive)
-    df = pd.concat([pd.read_csv(filename, index_col=None, header=0) for filename in all_files], axis=0,
-                   ignore_index=True)
-    # saved in the current folder
-    df.to_csv("immo_collect.csv")
-    return df
+    if len(all_files) > 0:
+        df = pd.concat([pd.read_csv(filename, index_col=None, header=0) for filename in all_files], axis=0,
+                       ignore_index=True)
+        # saved in the current folder
+        df.to_csv("immo_collect.csv")
+        return df
+    return None
--- main.py
+++ main.py
@@ -18,10 +18,28 @@ class CustomThread(Thread):
         # check existence of the page
         init_connection(self.driver, url_appart_search.format(0), title="Immoweb", check_button=True)
 
-        for current_page in range(self.p_start, self.p_end, self.search_id):
-            donnees = load_page(self.driver, current_page, self.search_id)
+        for current_page in range(self.p_start, self.p_end):
+            donnees = self.get_page_data(current_page)
             save_data(donnees, current_page, self.search_id)
         self.driver.close()
+
+    def get_page_data(self, page_number: int) -> List[Dict]:
+        current_url = (url_appart_search if current_search_id == 0 else url_house_search).format(page_number)
+        collected_links = self.collect_links(current_url)
+        donnees = []
+        for url in collected_links:
+            data = collect_info(self.driver, url, self.search_id)
+            donnees.append(data)
+            print(data)
+        return donnees
+
+    def collect_links(self, current_url: str) -> List[str]:
+        # target the right page
+        self.driver.get(current_url)
+        # take all the links
+        soup = BeautifulSoup(self.driver.page_source, "lxml")
+        intermediate_links = soup.find_all("a", {"class": "card__title-link"})
+        return [link.get("href") for link in intermediate_links]
 
 
 if __name__ == '__main__':
@@ -48,7 +66,7 @@ if __name__ == '__main__':
     """
 
     current_search_id = 1
-    # max pages to process
+    # min/max pages to process
     page_start = 0
     page_end = 333  # not included
     plage_page = [(0, 50), (50, 100)]
@@ -69,4 +87,5 @@ if __name__ == '__main__':
     ######################################
 
     csv = concat_all_csv("./immo-data/*.csv")
-    print(csv.describe())
+    if csv is not None:
+        print(csv.describe())
